ac093f98a98dd9f6c2fb78f4334c9ac8c3ca10f1
AlesanaOzkar <oscar.rubio_s@utzmg.edu.mx>
2021-02-23 14:49:10 -0600

    Version 1.0

--- public/js/index.js
+++ public/js/index.js
@@ -1,113 +1,116 @@
-const estado_form = document.getElementById('estado_form');
-const template_estado = document.getElementById('template_estado');
-const estados = document.getElementById('estados');
+const tarea_form = document.getElementById('tarea_form');
+const template_estado = document.getElementById('template_estado').content;
+const listaTareas = document.getElementById('tareas');
+const inputTexto = document.getElementById('input_texto');
+const fragment = document.createDocumentFragment();
  
-window.addEventListener('load',(evt)=>{ 
-     renderizarTodo(); 
+
+const tareas = {}
+
+window.addEventListener('DOMContentLoaded', (evt) => {
+    pintarTareas();
 });
- 
-function onClick(evt){ 
-    evt.preventDefault() 
-    if(evt.target.id === 'publicar_estado'){
-        //Obtener datos
-        const relevancia = document.getElementById('relevancia').value;
-        const input_estado = document.getElementById('input_estado').value;
-        //Crear estado
-        const estado = {
-            id: new Date().getTime(),
-            estado: input_estado,
-            relevancia,
-            fecha: new Date()
-        } 
-        //Obtener todos los estados
-        const estados_almacenados = JSON.parse(localStorage.getItem('estados'));
-        
-        if (!estados_almacenados) 
-            localStorage.setItem('estados',JSON.stringify( [...[],estado]))
-        else
-            localStorage.setItem('estados',JSON.stringify( [...estados_almacenados,estado]  ))
-   
-        renderizarTodo();
-    }   
- 
-} 
 
-function onDelete(evt){
-    const { id } = evt.target.dataset;
+function onSubmit(evt) {
+    evt.preventDefault()
+    //Obtener datos 
+    const texto = inputTexto.value;
 
-    const estados = JSON.parse(localStorage.getItem('estados'));
 
-    estados.forEach((v,i)=>{
-        if(v.id == id)
-            estados.splice(i,1); 
-    }) 
-       localStorage.setItem('estados',JSON.stringify(estados)); 
-}
+    if (texto.trim() === '')
+        return false;
 
-estados.addEventListener('click',(evt)=>{
-
-    if (evt.target.classList.contains('btn-eliminar')) {
-        onDelete(evt);
+    //Crear tarea
+    const tarea = {
+        id: Date.now(),
+        texto,
+        estado: false,
+        fecha: new Date()
     }
-
-
-})
-
-function renderizarEstado({id,relevancia,estado,fecha}){
-    return `
-        <div class="row">
-            <div class="col-12 col-sm-12 col-md-8 offset-md-2">
-                <div class="alert ${colorEstado(relevancia)}" role="alert">
-                    <div class="d-flex justify-content-between">
-                        <h4 class="alert-heading">${numeroEstado(relevancia)} </h4>
-                        <button class="btn-eliminar" data-id="${id}" data-relevancia="${relevancia}" >Eliminar</button>
-                    </div>
-                    <p>${estado}</p>
-                    <hr>
-                    <p class="mb-0">Publicado el ${fecha.toString().split('T')[0]}.</p>
-                </div>
-            </div>
-        </div> 
-    `;
-}
  
-function renderizarTodo(){
-   
-    let estados_almacenados = JSON.parse(localStorage.getItem('estados'));
+    tareas[tarea.id] = tarea;
 
-    if(estados_almacenados){
-        estados_almacenados = estados_almacenados.sort((a,b)=>{
-            if(new Date(a.fecha).getTime() > new Date(b.fecha).getTime())
-                return -1;
-            if(new Date(a.fecha).getTime() < new Date(b.fecha).getTime())
-                return 1;
-        }) 
-        let texto = ''; 
-        estados_almacenados.forEach(a => { 
-            texto+= renderizarEstado(a); 
-        }); 
-        estados.innerHTML = texto;
-    } 
-  
+    //Reinicia el formulario
+    evt.target.reset();
+    //Foco en el input
+    inputTexto.focus();
+    //Guardar tareas 
+    guardarTarea( );
+    //Muestra las tareas
+    pintarTareas();
 }
 
-estado_form.addEventListener("click",onClick);  
+tarea_form.addEventListener("submit", onSubmit);
+listaTareas.addEventListener("click", (evt) => {
+    if(evt.target.classList.contains('btn-actualizar')){
+        actualizarTarea(evt.target.dataset.id)
+        evt.stopPropagation();
+    }
+    if(evt.target.classList.contains('btn-eliminar')){
+        if(confirm('¿Deseas eliminar la tarea?')){
+            eliminarTarea(evt.target.dataset.id)
+            evt.stopPropagation();
+        }
+    }
+});
+
+const tareasGuardadas = () => {
+    return JSON.parse(localStorage.getItem('tareas'));
+}  
+
+const pintarTareas = () => {
+
+    //Elimina el contenido del DOM
+    listaTareas.innerHTML = '';
+
+    const tareas_locales = JSON.parse(localStorage.getItem('tareas'));
+
+    Object.values(tareas_locales).forEach(tarea => {
+        const templateClone = template_estado.cloneNode(true);
 
 
-const numeroEstado = (num) => {
-    return ['Triste','Normal','Feliz'][num-1]
-}; 
+        const textoTemplate = templateClone.querySelector('p'); 
+        textoTemplate.textContent = tarea.texto;
 
-const numeroEstado2 = num => ['Triste','Normal','Feliz'][num-1];
-  
-function colorEstado(estado){ 
-    if(estado == 1){
-        return 'alert-danger';
-    } 
-    else if (estado == 2){
-        return 'alert-success';
-    } 
-    else if (estado == 3){
-        return 'alert-warning';
-    } 
+        if(tarea.estado){
+            textoTemplate.classList.add('line-through');
+            templateClone.querySelector('.alert').classList.replace('alert-primary','alert-info')
+            templateClone.querySelector('.alert').classList.replace('alert-primary','alert-info') 
+            templateClone.querySelector('h3 i.btn-actualizar').classList.replace('fa-check-circle','fa-undo')
+ 
+
+        }else
+            textoTemplate.classList.remove('line-through');
+
+         
+        templateClone.querySelector('h3 i.btn-actualizar').dataset.id = tarea.id;
+        templateClone.querySelector('h3 i.btn-eliminar').dataset.id = tarea.id;
+ 
+        fragment.appendChild(templateClone);
+    });
+
+    listaTareas.appendChild(fragment)
+
+}
+
+const guardarTarea = ( ) => {
+    //Tareas guardadas 
+    const tareas_locales = tareasGuardadas();
+    //Guarda las tareas actuales
+    localStorage.setItem('tareas',JSON.stringify({...tareas_locales, ...tareas}))
+};
+
+
+const actualizarTarea = id => {
+    const tareas_locales = tareasGuardadas(); 
+    tareas_locales[id].estado = !tareas_locales[id].estado; 
+    localStorage.setItem('tareas',JSON.stringify({...tareas_locales})) 
+    pintarTareas();
+}
+
+const eliminarTarea = id => {
+    const tareas_locales = tareasGuardadas();   
+    delete tareas_locales[id];   
+    localStorage.setItem('tareas',JSON.stringify({...tareas_locales})) 
+    pintarTareas();
 }
